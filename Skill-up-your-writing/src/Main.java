bd5912cc23c1231d555148cb19fb078e47b5338e
tjalf.caesar <tjalf.caesar@EEG243.swnor.eeg-powerbox.de>
2017-03-21 13:25:09 +0100

    Right direction

--- Skill-up-your-writing/src/Main.java
+++ Skill-up-your-writing/src/Main.java
@@ -1,10 +1,21 @@
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
-		System.out.println ("Hello World");
-
+		      byte buffer[] = new byte[80];  // Zeichenpuffer
+		      String input = "";
+		      int read;
+		     
+		          // Einlesen der Zeichen
+		          read = System.in.read(buffer, 0, 80);
+		          // Umwandeln des Pufferinhaltes in einen String
+		          input = new String(buffer, 0, read);
+		          // Ausgabe der eingelesenen Zeichen
+		          System.out.print("I didn't get you!");
+		       
+		
 	}
 
 }
